7c9e65b1c7dafbb6004d49d3430f52b2500ea14a
elementary-221b <michael.watson_26@yahoo.com>
2026-01-31 20:50:07 -0500

    Update copy-code.js

--- assets/js/copy-code.js
+++ assets/js/copy-code.js
@@ -1,19 +1,18 @@
-document.querySelectorAll('pre').forEach((codeBlock) => {
-    // Create the button
-    const button = document.createElement('button');
-    button.className = 'copy-code-button';
-    button.type = 'button';
-    button.innerText = 'Copy';
+document.addEventListener('DOMContentLoaded', () => {
+    document.querySelectorAll('pre').forEach((codeBlock) => {
+        const button = document.createElement('button');
+        button.className = 'copy-code-button';
+        button.type = 'button';
+        button.innerText = 'Copy';
 
-    // The event: When clicked, move text to clipboard
-    button.addEventListener('click', () => {
-        const code = codeBlock.querySelector('code').innerText;
-        navigator.clipboard.writeText(code).then(() => {
-            button.innerText = 'Copied!';
-            setTimeout(() => { button.innerText = 'Copy'; }, 2000);
+        button.addEventListener('click', () => {
+            const code = codeBlock.querySelector('code').innerText;
+            navigator.clipboard.writeText(code).then(() => {
+                button.innerText = 'Copied!';
+                setTimeout(() => { button.innerText = 'Copy'; }, 2000);
+            });
         });
-    });
 
-    // Append the button to the pre-block
-    codeBlock.appendChild(button);
+        codeBlock.appendChild(button);
+    });
 });
