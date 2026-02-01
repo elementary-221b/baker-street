9a42778cad7da67d7225896a687f9b95a2f8e1b4
elementary-221b <michael.watson_26@yahoo.com>
2026-01-31 21:02:19 -0500

    Update copy-code.js

--- assets/js/copy-code.js
+++ assets/js/copy-code.js
@@ -1,18 +1,25 @@
-document.addEventListener('DOMContentLoaded', () => {
-    document.querySelectorAll('pre').forEach((codeBlock) => {
-        const button = document.createElement('button');
-        button.className = 'copy-code-button';
-        button.type = 'button';
-        button.innerText = 'Copy';
+document.addEventListener('DOMContentLoaded', function() {
+  // We look for the 'pre' blocks that Jekyll's kramdown creates
+  document.querySelectorAll('pre').forEach(function(codeBlock) {
+    
+    // Create the button
+    var button = document.createElement('button');
+    button.className = 'copy-code-button';
+    button.type = 'button';
+    button.innerText = 'Copy';
 
-        button.addEventListener('click', () => {
-            const code = codeBlock.querySelector('code').innerText;
-            navigator.clipboard.writeText(code).then(() => {
-                button.innerText = 'Copied!';
-                setTimeout(() => { button.innerText = 'Copy'; }, 2000);
-            });
-        });
-
-        codeBlock.appendChild(button);
+    // Click event to snatch the text
+    button.addEventListener('click', function() {
+      var code = codeBlock.querySelector('code').innerText;
+      navigator.clipboard.writeText(code).then(function() {
+        button.innerText = 'Copied!';
+        setTimeout(function() {
+          button.innerText = 'Copy';
+        }, 2000);
+      });
     });
+
+    // Append the button to the pre block
+    codeBlock.appendChild(button);
+  });
 });
