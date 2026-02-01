cffff3fc9356e8cff144f23de53c7e207d467723
elementary-221b <michael.watson_26@yahoo.com>
2026-02-01 10:33:22 -0500

    Delete _plugins/env_vars.rb

--- _plugins/env_vars.rb
+++
@@ -1,8 +0,0 @@
-module Jekyll
-  class EnvVarGenerator < Generator
-    def generate(site)
-      # This pulls the secret from the build environment into Jekyll
-      site.config['vt_api_key'] = ENV['VT_API_KEY']
-    end
-  end
-end
